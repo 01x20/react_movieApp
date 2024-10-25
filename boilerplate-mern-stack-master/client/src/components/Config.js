6bfbbfbc3a115bca6af28414139a52dbbc4e5793
kimgaeul <ge950120@gmail.com>
2024-10-25 17:51:34 +0900

    따라하며 배우는 노드, 리액트 시리즈 - 영화 사이트 만들기
    - boilerplate 사용으로 영화 사이트 만들기 강의 학습 중 (5/17)

---
+++ boilerplate-mern-stack-master/client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '401c09f7d0c44bcd588136d0aa843b81';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
