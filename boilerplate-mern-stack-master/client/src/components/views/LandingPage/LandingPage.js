af12c342a58d4ac321b81d67a698c87c30d61f2f
kimgaeul <ge950120@gmail.com>
2024-10-28 14:30:06 +0900

    loadMore 버튼 추가
    - fetch 중복 부분 fetchMovies 함수 처리
    - 현재 page state 추가 (CurrentPage, setCurrentPage)
    - 버튼 누를 때 다시 그려주는 게 아닌 20개 추가 붙임 처리
    setMovies([...Movies, ...response.results])

--- boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/LandingPage/LandingPage.js
@@ -10,21 +10,32 @@ function LandingPage() {
 
     const [Movies, setMovies] = useState([])
     const [MainMovieImage, setMainMovieImage] = useState(null)
+    const [CurrentPage, setCurrentPage] = useState(0)
 
 
     useEffect(() => {
         const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
         
-        fetch(endpoint)
-        .then (response => response.json())
-        .then (response => {
-
-            console.log([response.results]);
-            setMovies([...response.results])
-            setMainMovieImage(response.results[0])
-        })
-
+        fetchMovies(endpoint)
         }, [])
+
+        const loadMoreItems = () => {
+            const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${CurrentPage+1}`;
+
+            fetchMovies(endpoint)
+        }
+
+        const fetchMovies = (endpoint) => {
+            fetch(endpoint)
+            .then (response => response.json())
+            .then (response => {
+
+                console.log([response.results]);
+                setMovies([...Movies, ...response.results])
+                setMainMovieImage(response.results[0])
+                setCurrentPage(response.page)
+            })
+        }
         
     return (
         <div style={{width: '100%', margin: '0'}}>
@@ -60,7 +71,7 @@ function LandingPage() {
             </div>
 
             <div style={{display: 'flex', justifyContent: 'center'}}>
-                <button>Load More</button>
+                <button onClick={loadMoreItems}>Load More</button>
             </div>
         </div>
     );
